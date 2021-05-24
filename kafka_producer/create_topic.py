18fdef138f4e5186c41b9348ec4cc066778f2f23
bsotob <bsoto@externos.itnow.es>
2021-05-24 19:51:53 +0200

    initial commit

--- kafka_producer/create_topic.py
+++ kafka_producer/create_topic.py
@@ -1,12 +1,12 @@
 from confluent_kafka import Producer
 import socket
 
-conf = {'bootstrap.servers': "localhost:9092"}
+conf = {'bootstrap.servers': "0.0.0.0:9092"}
 
 producer = Producer(conf)
 
 
-producer.produce("NEW_TOPIC", key="key_name" , value="value_name")
+producer.produce("ropa", key="bamba" , value="nike")
 
 producer.flush()
 
